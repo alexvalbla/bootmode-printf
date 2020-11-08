fbfdc32d999ee635842386a20a7de1b8f2c38b5c
Alexandre Valencia <alex.valbla@gmail.com>
2020-11-08 03:06:40 +0100

    protected header

--- print.h
+++ print.h
@@ -1,3 +1,7 @@
+#ifndef LIBPRINTF
+#define LIBPRINTF
+
+
 int VSNPRINTF(char *str, size_t size, const char *format, va_list ap);
 
 int VSPRINTF(char *str, const char *format, va_list ap);
@@ -9,3 +13,6 @@ int SNPRINTF(char *str, size_t size, const char *format, ...);
 int SPRINTF(char *str, const char *format, ...);
 
 int PRINTF(const char *format, ...);
+
+
+#endif
