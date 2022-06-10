ceddc05fa099e7f979b78d475e5d63538f0f717e
Alexandre Valencia <alex.valbla@gmail.com>
2022-06-10 16:18:25 +0200

    fixed minor output error

--- tests/test_strings.c
+++ tests/test_strings.c
@@ -35,7 +35,7 @@ int main(int argc, char **argv) {
 
 
         char c = 'z';
-        const char *fmt = "inserting charater right '%c' <- here";
+        const char *fmt = "inserting character right '%c' <- here";
         sprintf(output1, fmt, c);
         bootmode_sprintf(output2, fmt, c);
         if (strcmp(output1, output2)) {
