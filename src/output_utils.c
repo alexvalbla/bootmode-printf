f3ed1db8c3e1c3c857d7d01597bd2f11caac72e9
Alexandre Valencia <alex.valbla@gmail.com>
2022-07-04 20:58:41 +0200

    refactored output_utils code

--- src/output_utils.c
+++ src/output_utils.c
@@ -22,58 +22,25 @@ void output_char(bm_output_ctxt *ctxt, char c) {
         if (ctxt->output_str) {
                 // outputting to string
                 if (!(ctxt->flags&FLAG_LIMIT) || ctxt->total_written < ctxt->limit_n) {
-                        ctxt->output_str[ctxt->total_written] = c;
-                        ++(ctxt->total_written);
+                        ctxt->output_str[ctxt->total_written++] = c;
                 }
         } else {
                 // outputting to stdout
-                ++(ctxt->total_written);
                 bm_putchar(c);
         }
         ++(ctxt->total_needed);
 }
 
 void output_char_loop(bm_output_ctxt *ctxt, char c, size_t nb_times) {
-        size_t nb_times_trunc = nb_times;
-        if (ctxt->output_str) {
-                // outputting to string
-                size_t space_left = ctxt->limit_n - ctxt->total_written;
-                if ((ctxt->flags&FLAG_LIMIT) && space_left < nb_times) {
-                        nb_times_trunc = space_left;
-                }
-                for (size_t i = 0; i < nb_times_trunc; ++i) {
-                        ctxt->output_str[ctxt->total_written+i] = c;
-                }
-        } else {
-                // outputting to stdout
-                for (size_t i = 0; i < nb_times_trunc; ++i) {
-                        bm_putchar(c);
-                }
+        for (size_t i = 0; i < nb_times; i++) {
+                output_char(ctxt, c);
         }
-        ctxt->total_written += nb_times_trunc;
-        ctxt->total_needed += nb_times;
 }
 
 void output_buffer(bm_output_ctxt *ctxt, const char *buffer, size_t buff_sz) {
-        size_t loop_length = buff_sz;
-        if (ctxt->output_str) {
-                // outputting to string
-                size_t space_left = ctxt->limit_n - ctxt->total_written;
-                if ((ctxt->flags&FLAG_LIMIT) && space_left < buff_sz) {
-                        loop_length = space_left;
-                }
-                for (size_t i = 0; i < loop_length; ++i) {
-                        ctxt->output_str[ctxt->total_written+i] = buffer[i];
-                }
-        } else {
-                // outputting to stdout
-                for (size_t i = 0; i < loop_length; ++i) {
-                        bm_putchar(buffer[i]);
-                }
-
+        for (size_t i = 0; i < buff_sz; i++) {
+                output_char(ctxt, buffer[i]);
         }
-        ctxt->total_written += loop_length;
-        ctxt->total_needed += buff_sz;
 }
 
 void append_nul(bm_output_ctxt *ctxt) {
@@ -84,7 +51,7 @@ void append_nul(bm_output_ctxt *ctxt) {
                         ctxt->output_str[ctxt->total_written] = '\0';
                 } else if (ctxt->limit_n > 0) {
                         if (ctxt->total_written < ctxt->limit_n) {
-                                // there is still roon for terminating '\0'
+                                // there is still room for terminating '\0'
                                 ctxt->output_str[ctxt->total_written] = '\0';
                         } else {
                                 // overwrite last written character
